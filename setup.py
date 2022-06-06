5a0ec30f0eb0fef9d762c351e43e850d95a9ac43
jasleen1602 <84179720+jasleen1602@users.noreply.github.com>
2022-06-06 17:41:00 +0530

    edit

---
+++ setup.py
@@ -0,0 +1,9 @@
+import numpy as np
+#import preprocessor as p
+import counselor
+from tensorflow.keras.models import load_model
+import joblib
+from pathlib import Path
+import nltk
+import pandas as pd
+nltk.download('wordnet')
