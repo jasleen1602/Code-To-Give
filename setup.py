ac89ff37a1a458f3caef8e5401fcf96e50a14e10
Jasleen Kaur <jasleenkaur1602@gmail.com>
2022-06-08 23:34:40 +0530

    Delete setup.py

--- setup.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-#import preprocessor as p
-import counselor
-from tensorflow.keras.models import load_model
-import joblib
-from pathlib import Path
-import nltk
-import pandas as pd
-nltk.download('wordnet')
